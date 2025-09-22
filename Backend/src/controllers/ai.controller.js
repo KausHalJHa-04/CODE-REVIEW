f545462d097c31e72083974f5693668b178f9902
kaushal kumar jha <jhakaushal361@gmail.com>
2025-09-23 03:15:35 +0530

    sixth commit

--- Backend/src/controllers/ai.controller.js
+++ Backend/src/controllers/ai.controller.js
@@ -9,12 +9,9 @@ module.exports.getReview = async (req, res) => {
         return res.status(400).send("Prompt is required");
     }
 
-    try {
-        const response = await aiService(code);
-        res.send(response);
-    } catch (error) {
-        console.error("Error in getReview controller:", error);
-        // Send a generic server error message to the client
-        res.status(500).send("An error occurred while processing your request.");
-    }
+    const response = await aiService(code);
+
+
+    res.send(response);
+
 }
